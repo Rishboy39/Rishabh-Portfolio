9f0b86e387dbdeb46f8f04fa6d3403594bfb785c
Rishabh Menasinakayi <160802022+Rishboy39@users.noreply.github.com>
2024-10-03 15:15:58 -0700

    Add files via upload

---
+++ components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Rishabh Menasinakayi </span>
+            from <span className="purple"> Redmond, Washington.</span>
+            <br />
+            I am currently studying High School
+            <br />
+            I have completed Elementary and middle school in India and moved to America during High School
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Sports
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            {" "}
+          </p>
+          <footer className="blockquote-footer"></footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
